3261418c819f89f10221824c5c741b7d8ef9df52
Nathan Garcia <n8g423@gmail.com>
2025-01-04 23:48:18 -0700

    Update layout.tsx

--- cc-v0/app/layout.tsx
+++ cc-v0/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-import { Geist, Azeret_Mono as Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 
 const geistSans = Geist({
