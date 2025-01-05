6b8d635c4667c51b6baff290f7258ebae45bcd98
Nathan Garcia <n8g423@gmail.com>
2025-01-05 00:51:01 -0700

    Update layout.tsx

--- cc-v0/app/layout.tsx
+++ cc-v0/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
+
 import './globals.css'
 
 const geistSans = Geist({
@@ -31,6 +32,7 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="system"
           enableSystem
+          disableTransitionOnChange
         >
           {children}
         </ThemeProvider>
