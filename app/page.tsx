0579f98d258c9dcd50841406490703a486ec004a
Nathan Garcia <n8g423@gmail.com>
2025-03-09 13:40:56 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import { ChevronDown, ChevronUp } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+// import Link from "next/link";
 
 const people = [
   {
