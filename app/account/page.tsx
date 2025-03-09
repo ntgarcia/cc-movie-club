7ab66e87460360815dc58761e0024f2c83a4a895
Nathan Garcia <n8g423@gmail.com>
2025-03-09 13:34:03 -0600

    Update page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,11 +2,9 @@
 import { createClient } from "@/utils/supabase/server";
 
 export default async function Account() {
-  const supabase = await createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
+  // const supabase = await createClient();
+  // const {
+  //   data: { user },
+  // } = await supabase.auth.getUser();
   // return <AccountForm user={user} />;
 }
